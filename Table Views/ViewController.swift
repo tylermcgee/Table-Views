244db0740e2f4efef3fb41f333f07428751d288a
Tyler McGee <tmcgee2481@gmail.com>
2017-06-28 09:27:26 -0400

    Committed 6/28/17.

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -8,7 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var cellContent = ["Tyler", "Adam", "Katie", "Ryan"]
+
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellContent.count
+        
+    }
+    
+
+    
+
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = cellContent[indexPath.row]
+        
+        return cell
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
